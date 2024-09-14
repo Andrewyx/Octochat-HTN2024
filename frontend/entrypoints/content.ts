9a59b10f14a13b75bcc5165c8fd51be37045d8b8
elisabeth <lishyl26@gmail.com>
2024-09-14 18:24:32 -0400

    edited content to act on github instead of google

--- frontend/entrypoints/content.ts
+++ frontend/entrypoints/content.ts
@@ -1,6 +1,16 @@
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
+  matches: ['*://*.github.com/*/*'],
   main() {
     console.log('Hello content.');
+    (browser.action ?? browser.browserAction).onClicked.addListener(
+      async (tab) => {
+        try {
+          const dataUrl = await browser.tabs.captureTab();
+          console.log(dataUrl);
+        } catch (err) {
+          console.error("Cannot get URL current tab", tab, err);
+        }
+      },
+    );
   },
 });
