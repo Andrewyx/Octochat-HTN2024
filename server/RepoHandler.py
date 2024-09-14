35a69869b4ce8de33ade07c6f7634ca035aa8351
Andrew Mao <andrewyxmao@gmail.com>
2024-09-14 16:39:42 -0400

    changes

--- server/RepoHandler.py
+++ server/RepoHandler.py
@@ -1,5 +1,7 @@
-def get_repo() -> None:
-    pass
+from Constants import DATA_FOLDER_PATH 
+
+def get_repo(repo_url) -> None:
+    repo = Repo.clone_from(repo_url, DATA_FOLDER_PATH)
 
 def parse_downloaded_repo() -> None:
     pass
