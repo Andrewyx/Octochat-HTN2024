7d23cdc2fc89854e1bac687f00baf85e39ff7979
Andrew Mao <andrewyxmao@gmail.com>
2024-09-14 15:49:28 -0400

    backend started

---
+++ server/RepoHandler.py
@@ -0,0 +1,11 @@
+def get_repo() -> None:
+    pass
+
+def parse_downloaded_repo() -> None:
+    pass
+
+def is_valid_code_file(fileHeader: str) -> bool:
+    return True
+
+def rename_to_txt() -> None:
+    pass
--- server/app.py
+++ server/app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from VoiceflowHandler import delete_cache
 
 app = Flask(__name__)
+cachedURL  = ""
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/parseurl", methods=['POST'])
+def recieve_url() -> None:
+    url = request.args.get("url")
+    if url == "":
+        raise ValueError("Empty URL")
+    elif not url_is_cached(url):
+        delete_cache()
+        cachedURL = url
+    # ELSE: DO NOT DO ANYTHING AND DO NOT DISPLAY INJECTED BUTTON
+
+def url_is_cached(url: str) -> bool:
+    return cachedURL.strip() == url.strip()
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, port=5525, threaded=True, host="0.0.0.0", debug=True)
+    
+
+
+
+
