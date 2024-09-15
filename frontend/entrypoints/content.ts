313d7c4ecf223cac2e0b88fe68b19ceb825b5b89
Kezia <roselinakeziarijadi@gmail.com>
2024-09-14 22:19:59 -0400

    whoopsies

--- frontend/entrypoints/content.ts
+++ frontend/entrypoints/content.ts
@@ -1,17 +1,7 @@
 export default defineContentScript({
-<<<<<<< HEAD
   matches: ['*://*.github.com/*/*'],
   main() {
     console.log('Hello content.');
     console.log(window.location.href);
-=======
-  matches: ["*://*/*"],
-  async main() {
-    console.log("Injecting script...");
-    await injectScript('/injected.js', {
-      keepInDom: true,
-    });
-    console.log("Done!");
->>>>>>> f36fe19 (loading in chat)
   },
 });
